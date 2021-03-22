ad208bea2ebe218f08ce24b4f1c0da891cd8f53c
Jerrick <jerrickzhu@berkeley.edu>
2021-03-21 21:20:51 -0700

    text scrapper working properly on test case

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cheerio = require("cheerio");
 const axios = require("axios");
-const request = require("request");
 const app = express();
 const port = 4000;
 
@@ -9,21 +8,13 @@ app.set('view engine');
 
 app.get('/', (req, res) => {
     const url = "https://www.nytimes.com/2016/06/10/movies/the-conjuring-2-review.html";
-    if (url) {
-        console.log("now scraping: " + url);
-        request(url, function(err, response, html) {
-            let data;
-            let title;
-            if (!err) {
-                var $ = cheerio.load(html);
-                title = $('h1');
-                data = $("[name=articleBody]").html();
-            }
-            res.send(data);
-        })
-    } else {
-        res.send("error!");
-    }
+    axios.get(url)
+    .then(response => {
+        const $ = cheerio.load(response.data);
+        const data = $('.StoryBodyCompanionColumn').text()
+        res.send(data);
+
+    });
     
 })
 
